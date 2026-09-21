6df4c9dcb6906375b406d84ad0970720faf67ee8
omvinayaktripathy-hub <omvinayakwork@gmail.com>
2026-09-21 10:19:43 +0530

    Stop tracking checkAdmin.js

--- checkAdmin.js
+++
@@ -1,7 +0,0 @@
-const { initializeApp, cert } = require("firebase-admin/app");
-const { getAuth } = require("firebase-admin/auth");
-const sa = require("./serviceAccountKey.json");
-initializeApp({ credential: cert(sa) });
-
-getAuth().getUser("Aqooz7q3RUObUNGgdnBYaiZ2Xte2")
-  .then(u => { console.log("Claims:", u.customClaims); process.exit(0); });
